4c1ccd96cbfe2f731c53b7a1ee9b1b1f07fe8126
Hendrix Peralta <hendrixperalta@gmail.com>
2025-05-02 16:30:18 +0900

    Standardized the images - The pixels now have values between 1 and 0

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os
 import cv2 
 from PIL import Image 
 import numpy as np 
+from patchify import patchify
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+# %%
+minmaxscaler = MinMaxScaler()
 # %%
 dataset_root_folder = r"C:\Users\hendr\Desktop\programming\CNN-Dubai\resources"
 # %%
@@ -19,9 +24,16 @@ for path, subdirs, files in os.walk(os.path.join(dataset_root_folder,dataset_nam
                 print(image_name)        
 # %%
 image = cv2.imread(f"{dataset_root_folder}/{dataset_name}/Tile 2/images/image_part_001.jpg",1)
- 
+
 # %%
 patch_size = 256
+
+# %%
+image_patches = patchify(image, (patch_size, patch_size, 3), step=patch_size) 
+image_patches
+# %%
+len(image_patches)
+# %%
 image.shape[0]//patch_size*patch_size
 # %%
 print(type(image))
@@ -40,11 +52,28 @@ for tile_id in range(1,8):
             #print(image.shape)
             size_x = (image.shape[0]//patch_size)*patch_size
             size_y = (image.shape[1]//patch_size)*patch_size
-            print("{} === {} - {}".format(image.shape, size_x, size_y))
+            #print("{} === {} - {}".format(image.shape, size_x, size_y))
             image = Image.fromarray(image)
             image = image.crop((0, 0, size_x, size_y))
-            print("{}, {}".format(image.size[0],image.size[1]))
+            #print("{}, {}".format(image.size[0],image.size[1]))
             
+            #Converting back to an array 
+            image = np.array(image)
+            image_patches = patchify(image, (patch_size, patch_size,3), step=patch_size)
+            
+            #selecting the images from the patches 
+            for i in range(image_patches.shape[0]):
+                for j in range(image_patches.shape[1]):
+                    individual_patched_images = image_patches[i,j,:,:]
+                    # print(individual_patched_images.shape)
+                    
+                    # MinMaxing 
+                    individual_patched_images = minmaxscaler.fit_transform(individual_patched_images.reshape(-1, individual_patched_images.shape[-1])).reshape(individual_patched_images.shape)
+                    
+                    individual_patched_images = individual_patched_images[0]
+                    # print(individual_patched_images.shape)
+                    image_dataset.append(individual_patched_images)
+                    
 # %%
-image_dataset
+print(len(image_dataset))
 # %%
