e5488723750601307f75d750552420df226e6196
Hendrix Peralta <hendrixperalta@gmail.com>
2025-05-02 16:50:14 +0900

    Edited masks - the masks were patched and minmaxed. the resulting images were stored in the mask_dataset

--- main.py
+++ main.py
@@ -42,38 +42,50 @@ print(type(Image.fromarray(image)))
 
 # %%
 image_dataset = []
+mask_dataset = []
 
-image_type = "images" # "masks"
-image_extension = "jpg" # "PNG"
-for tile_id in range(1,8):
-    for image_id in range(1,20):
-        image = cv2.imread(f"{dataset_root_folder}/{dataset_name}/Tile {tile_id}/{image_type}/image_part_00{image_id}.{image_extension}")
-        if image is not None: 
-            #print(image.shape)
-            size_x = (image.shape[0]//patch_size)*patch_size
-            size_y = (image.shape[1]//patch_size)*patch_size
-            #print("{} === {} - {}".format(image.shape, size_x, size_y))
-            image = Image.fromarray(image)
-            image = image.crop((0, 0, size_x, size_y))
-            #print("{}, {}".format(image.size[0],image.size[1]))
-            
-            #Converting back to an array 
-            image = np.array(image)
-            image_patches = patchify(image, (patch_size, patch_size,3), step=patch_size)
-            
-            #selecting the images from the patches 
-            for i in range(image_patches.shape[0]):
-                for j in range(image_patches.shape[1]):
-                    individual_patched_images = image_patches[i,j,:,:]
-                    # print(individual_patched_images.shape)
-                    
-                    # MinMaxing 
-                    individual_patched_images = minmaxscaler.fit_transform(individual_patched_images.reshape(-1, individual_patched_images.shape[-1])).reshape(individual_patched_images.shape)
-                    
-                    individual_patched_images = individual_patched_images[0]
-                    # print(individual_patched_images.shape)
-                    image_dataset.append(individual_patched_images)
+for image_type in ["images" , "masks"]:
+    if image_type == "images":
+        image_extension = "jpg" 
+    else: 
+        image_extension = "png" 
+    for tile_id in range(1,8):
+        for image_id in range(1,20):
+            image = cv2.imread(f"{dataset_root_folder}/{dataset_name}/Tile {tile_id}/{image_type}/image_part_00{image_id}.{image_extension}")
+            if image is not None: 
+                #print(image.shape)
+                size_x = (image.shape[0]//patch_size)*patch_size
+                size_y = (image.shape[1]//patch_size)*patch_size
+                #print("{} === {} - {}".format(image.shape, size_x, size_y))
+                image = Image.fromarray(image)
+                image = image.crop((0, 0, size_x, size_y))
+                #print("{}, {}".format(image.size[0],image.size[1]))
+                
+                #Converting back to an array 
+                image = np.array(image)
+                image_patches = patchify(image, (patch_size, patch_size,3), step=patch_size)
+                
+                #selecting the images from the patches 
+                for i in range(image_patches.shape[0]):
+                    for j in range(image_patches.shape[1]):
+                        individual_patched_images = image_patches[i,j,:,:]
+                        # print(individual_patched_images.shape)
+                        
+                        # MinMaxing 
+                        individual_patched_images = minmaxscaler.fit_transform(individual_patched_images.reshape(-1, individual_patched_images.shape[-1])).reshape(individual_patched_images.shape)
+                        
+                        individual_patched_images = individual_patched_images[0]
+                        # print(individual_patched_images.shape)
+
+                        if image_type == "images":
+                            image_dataset.append(individual_patched_images)
+                        else: 
+                            mask_dataset.append(individual_patched_images)
+
                     
 # %%
 print(len(image_dataset))
 # %%
+print(len(mask_dataset))
+# %%
+ 
